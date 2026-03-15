f3dca697f7b3c99d5ffdda4a95937f6a86ad7645
Steven Cox <stevenjacox@outlook.com>
2026-03-14 21:19:08 -0700

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { EventsCalendarComponent } from './components/events-calendar/events-calendar';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, EventsCalendarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ASSBar');
+}
